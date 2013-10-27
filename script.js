c6a235ecd24ce7de66caa363ac5770f5405f402c
lucasw <wsacul@gmail.com>
2013-10-27 04:07:23 -0700

    Trying to cover 1 pixel gaps, also trying to avoid unnecessary drawing of walls.  Much more robust occlusion is possible of course.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // create a random map
 var map = new Array();
-
-for (var i = 0; i < 16; i++) {
+var map_sz = 32;
+for (var i = 0; i < map_sz; i++) {
   map[i] = new Array();
-  for (var j = 0; j < 16; j++) {
+  for (var j = 0; j < map_sz; j++) {
     //map[i][j] = (i%2 === 0);
     map[i][j] = Math.random() > 0.8;
   }
@@ -48,18 +48,21 @@ var drawFlatWall = function(the_canvas, i, j, color, x_dist, y_dist, sz,
     x_center, y_center,
     x_max) {
   
-  if (x_center < x_max) {
+  // isn't occluded directly
+  var not_occluded = (j === map.length - 1) || (!map[i][j+1]); 
+
+  if (not_occluded && (x_center + sz > 0) && (x_center < x_max)) {
   var new_id = 'map_3d_' + i + '_' + j;
   var new_div = '<div class="block" id=' + new_id + '></div>';
   the_canvas.append(new_div);
 
   $('#' + new_id).css('height', sz + 'px');
-  $('#' + new_id).css('width',  sz + 'px');
+  $('#' + new_id).css('width',  (sz + 2) + 'px');
   $('#' + new_id).css('background-color', color); 
   $('#' + new_id).css('z-index', -Math.round(y_dist * map.length));
 
   $('#' + new_id).css('left', 
-        (x_center) + 'px');
+        (x_center - 2) + 'px');
   $('#' + new_id).css('top',  
         (y_center) + 'px');
   }
@@ -67,22 +70,25 @@ var drawFlatWall = function(the_canvas, i, j, color, x_dist, y_dist, sz,
 
 var drawPerspectiveWall = function(the_canvas, i, j, 
     x_dist, y_dist, sz, sz_3d, 
-    x_offset_3d, x_3d_view_center,
+    y_offset_3d, x_3d_view_center,
     x_center, y_center,
     x_max) {
   // draw perspective walls
-  var color_trap = 'rgb(' 
-    + (255 - i * 16 - 50) + ',' + 
-    + (255 - j * 8 - 50)  + ',' + 
-    + ((i + j) * 4) + 
-    ')';
+
 
   var draw_right = ((x_dist > 0.5) && ((i === 0) || !(map[i-1][j])));
   var draw_left = ((x_dist < -0.5) && ((i === map.length-1) || !(map[i+1][j])));
   // draw right hand walls
   if (draw_right || draw_left) {
-    var sz2 = sz_3d / Math.pow(2, y_dist + 1);
-    var y_center2 = x_offset_3d + sz_3d/2 - sz2/2;
+    var y_pow = Math.pow(2, y_dist + 1);
+    var color_trap = 'rgb(' 
+      + ((255 - i * 16 - 50) ) + ',' + 
+      + (255 - j * 8 - 50)  + ',' + 
+      + ((i + j) * 4 ) + 
+      ')';
+
+    var sz2 = sz_3d / y_pow;
+    var y_center2 = y_offset_3d + sz_3d/2 - sz2/2;
     
     if (draw_right) {
       var x_center2 = (x_dist * sz2) - sz2/2 + x_3d_view_center;
@@ -106,7 +112,7 @@ var drawPerspectiveWall = function(the_canvas, i, j,
         drawTrapezoid(
             i, j,
             draw_right,
-            x, y_center, // ul xy  
+            x - 2.0, y_center, // ul xy  
             trap_width + 1.0, sz, // width + fudge, height
             sz/4,   // trap_height
             - Math.round(y_dist * map.length + Math.abs(x_dist)), // z-index
@@ -120,41 +126,44 @@ var drawScreen = function(x_pos, y_pos, the_canvas) {
   $(".top").empty();
   // draw the xy coordinates to the screen
   var x_max = $('body').innerWidth();
-  the_canvas.append('<p>' + x_max + ' ' + 
+  var y_max = $(window).height(); //$('body').innerHeight();
+  if (false) {
+  the_canvas.append('<p>' + x_max + ' ' + y_max + ' ' + 
       screen.width + ' ' + screen.height + ' ' + 
       x_pos + ' ' + y_pos + '</p>');
-  
-  var x_offset_3d = map.length * 20;
+  }
+
   var x_3d_view_center = 720;
-  var sz_3d = 512;
+  var sz_3d = Math.min(x_max, y_max);
+  var y_offset_3d = y_max/2 - sz_3d/2; // map.length * 20;
 
   for (var i = 0; i < map.length; i++) {
     for (var j = 0; j < map[i].length; j++) {
       if (map[i][j]) {
         var color = 'rgb(' 
-            + (255 - i * 16) + ',' + 
-            + (255 - j * 8)  + ',' + 
+            + (255 - i * 12) + ',' + 
+            + (255 - j * 6)  + ',' + 
             + ((i + j) * 4) + 
         ')';
 
         // 2d map view
-        drawMap(the_canvas, i, j, color);
+        //drawMap(the_canvas, i, j, color);
 
         // 3d view
         var x_dist = i - x_pos;
         var y_dist = y_pos - j;
 
-        if ((y_dist >= 0) && (y_dist < 8)) {
+        if ((Math.abs(x_dist) < 28) && (y_dist >= 0) && (y_dist < 7)) {
           var sz = sz_3d / Math.pow(2, y_dist);
           var x_center = (x_dist * sz) - sz/2 + x_3d_view_center;
-          var y_center = x_offset_3d + sz_3d/2 - sz/2;
+          var y_center = y_offset_3d + sz_3d/2 - sz/2;
           if ((y_dist > 0)) {
             // draw the straight on walls
             drawFlatWall(the_canvas, i, j, color, x_dist, y_dist, sz, 
                 x_center, y_center, x_max);
             
             drawPerspectiveWall(the_canvas, i, j, x_dist, y_dist, sz, sz_3d,
-                x_offset_3d, x_3d_view_center,
+                y_offset_3d, x_3d_view_center,
                 x_center, y_center,
                 x_max);
           } // y_dist
@@ -165,18 +174,20 @@ var drawScreen = function(x_pos, y_pos, the_canvas) {
   } // map for loop x
 
   // draw the player position
-  // TBD make this into drawBlock function
-  var i = x_pos;
-  var j = y_pos;
-  var x = i * 20;
-  var y = j * 20;
-  if (x < screen.width) {
-  var new_id = 'map_player' + Math.round(i) + '_' + Math.round(j);
-  var new_div = '<div class="block" id=' + new_id + '></div>';
-  the_canvas.append(new_div);
-  $('#' + new_id).css('left', Math.round(x) + 'px');
-  $('#' + new_id).css('top',  Math.round(y) + 'px');
-  $('#' + new_id).css('background-color', "#001100"); 
+  if (false) {
+    // TBD make this into drawBlock function
+    var i = x_pos;
+    var j = y_pos;
+    var x = i * 20;
+    var y = j * 20;
+    if (x < screen.width) {
+      var new_id = 'map_player' + Math.round(i) + '_' + Math.round(j);
+      var new_div = '<div class="block" id=' + new_id + '></div>';
+      the_canvas.append(new_div);
+      $('#' + new_id).css('left', Math.round(x) + 'px');
+      $('#' + new_id).css('top',  Math.round(y) + 'px');
+      $('#' + new_id).css('background-color', "#001100"); 
+    }
   }
 }
 
